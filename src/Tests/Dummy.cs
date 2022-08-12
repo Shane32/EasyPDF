860e41819f5fd99f890557be4ef99ca614a3ae8d
Shane Krueger <shane@acdmail.com>
2022-08-11 23:43:51 -0400

    QR Code Update (#6)

--- src/Tests/Dummy.cs
+++ src/Tests/Dummy.cs
@@ -12,6 +12,9 @@ namespace EasyPDFTests
             writer.ScaleMode = ScaleModes.Inches;
             writer.NewPage(System.Drawing.Printing.PaperKind.Letter, false, 1, 1);
             writer.Write("Hello");
+            writer.QRCode("Test");
+            writer.CurrentX = 0;
+            writer.WriteLinesAt(true, 0f, 0f, 2f, 0f, 0f, "Testing", false);
             _ = writer.ToArray();
         }
     }
